10d4b4eebf56d74950fce7b640e9c86b98053c9b
Paul Mc Grath <G00398606@gmit.ie>
2021-03-27 10:42:39 +0000

    import pandas&numpy-run file to check installed

--- pands-project2021/analysis.py
+++ pands-project2021/analysis.py
@@ -1,8 +1,5 @@
 # analysis.py
 # Author: paul mcgrath
 
-<<<<<<< HEAD
-# test commit 
-=======
-# test commit
->>>>>>> f9574411a2a663ee8797b5add1ec5fbbbafd2533
+import pandas as pd 
+import numpy as np
