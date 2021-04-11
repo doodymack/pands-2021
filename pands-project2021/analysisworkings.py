c58be25393ffa09eed1ac7e6cddef4627c1e2c87
Paul Mc Grath <G00398606@gmit.ie>
2021-04-11 20:35:05 +0100

    BASIC STATS PROGRAM
    working on basic stats to get histograms of each attribute per species. once these can be created reseatch on the correlations.
    # only ###when these are working go back to code and look for potential for improvement

--- pands-project2021/analysisworkings.py
+++ pands-project2021/analysisworkings.py
@@ -28,16 +28,56 @@ print(data)
 #print data from rows 10 to 20
 slice = (data[10:21])
 print (slice)
-
-extractdata1= data[["Id","SepalLengthCm"]]
-print(extractdata1.head(15))
 '''
-# return the data from a specific row
+#extractdata1= data[["Id","SepalLengthCm"]]
+#print(extractdata1.head(15))
+
+#IrisSetosaData= data["Species","Iris-setosa"]
+#print(IrisSetosaData.head(15))
+
+# return the data from a specific Row
 # loc= index name of the row
 # iloc = index integer of the row
 
 #print (data.iloc [5])
-print(data.loc[data["Species"]=="Iris-setosa"])
+#print rows with specified data i.e. number.  Note can't use "4.7" as its not a string but integer
+# can also print all of one species if select by species -this time its a string so need "  "
+#print(data.loc[data["PetalLengthCm"]==4.8])
+
+# print data based on final column variable i.e. string (species)
+'''DataIS = (data.loc[data["Species"]=="Iris-setosa"])
+print(DataIS.head(15))
+
+DataIVI = (data.loc[data["Species"]=="Iris-virginica"])
+print(DataIVI.head(15))
+
+DataIVE = (data.loc[data["Species"]=="Iris-versicolor"])
+print(DataIVE.head(15))
+'''
+# The value_counts() function, counts the number of times a particular instance or data has occurred
+print (data["Species"].value_counts())
+
+# Calculating basic statisitics per column i.e. sum, mean,median, min & max
+# data["column_name"].x()  where "x" = sum/mean/median
+sumSepalLength= data["SepalLengthCm"].sum()
+meanSepalLength= data["SepalLengthCm"].mean()
+medianSepalLength= data["SepalLengthCm"].median()
+minSepalLength= data["SepalLengthCm"].min()
+maxSepalLength= data["SepalLengthCm"].max()
+
+# \n = new line if use \t = tab
+print ("Statistics for SepalLengthCm :")
+print("\nSum:",sumSepalLength, "\nMean:",meanSepalLength , "\nMedian:",
+medianSepalLength,"\nMin:",minSepalLength,"\nMax:",maxSepalLength)
+
+# find out  whats happening here   https://www.geeksforgeeks.org/python-basics-of-pandas-using-iris-dataset/
+
+data.isnull()
+print (data.isnull)
+
+# keep working on basic stats to get histograms of each attribute per species
+# once these can be created reseatch on the correlations.
+# only when these are working go back to code and look for potential for improvement
 '''
 # Create a Histogram for Sepal Length
 plt.figure(figsize = (10, 7))
